a9344373ce812f728f505676f49752b9a92942fa
DEBargha2004 <debarghasaha16@gmail.com>
2023-11-14 23:52:42 +0530

    init
    project initialised

---
+++ src/context/global-app-state-context.tsx
@@ -0,0 +1,4 @@
+import { createContext } from 'react'
+
+//@ts-ignore
+export const global_app_state_context = createContext()
---
+++ src/provider/global-app-state-provider.tsx
@@ -0,0 +1,12 @@
+import { global_app_state_context as GlobalAppStateContext } from '@/context/global-app-state-context'
+import { ReactNode } from 'react'
+
+const GlobalAppStateProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <GlobalAppStateContext.Provider value={{}}>
+      {children}
+    </GlobalAppStateContext.Provider>
+  )
+}
+
+export default GlobalAppStateProvider
